587112c6a59c179b09835ed94778c5945d45196f
bhagya <bhagya.bijoy@csr-informatik.de>
2025-07-01 23:41:42 +0530

    New folders for patterns with basic print statement is added.

--- Odd_or_evn_check.c
+++ Odd_or_evn_check.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int num;
-    printf("Enter the number to check whether odd or even:");
-    scanf("%d", &num);
-
-    // Using if-else   
-    // if(num%2==0)
-    // {
-    //     printf("%d is even", num);
-    // }
-    // else{
-    //      printf("%d is odd", num);
-    // }
-
-    // Using Ternary Operator
-    num%2==0?printf("%d is even", num):printf("%d is odd", num);
-    return 0;
+   int num;
+   printf("Enter a number:");
+   scanf("%d", &num);
+   if(num%2==0){
+        printf("Number is even");
+   }
+   else
+   {
+    printf("Number is odd");
+   }
+   return 0;
 }
---
+++ patterns using single print statement/rightHalfPyramid.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+  printf("*\n*\t*\n*\t*\t*\n*\t*\t*\t*\n*\t*\t*\t*\t*");
+  return 0;
+}
